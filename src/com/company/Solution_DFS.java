c2828fefcc3d08740b8899679ab8e0b007ba64f3
jjs <jj2903@nyu.edu>
2020-09-10 01:42:02 -0400

    many days work

--- src/com/company/Solution_DFS.java
+++ src/com/company/Solution_DFS.java
@@ -141,6 +141,211 @@ public class Solution_DFS {
         }
     }
 
+    public void Num130solve(char[][] board) {
+        if(board.length==0||board[0].length==0)
+            return;
+        int n = board.length;
+        int m = board[0].length;
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j< board[0].length;j++)
+            {
+                if(i==0||i==n-1||j==0||j==m-1)
+                {
+                    if(board[i][j]=='O')
+                    {
+                        Num130helper(board,i,j,n,m);
+                    }
+                }
+            }
+        }
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j< board[0].length;j++) {
+                if(board[i][j]=='O')
+                    board[i][j]='X';
+            }
+        }
+
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j< board[0].length;j++) {
+                if(board[i][j]=='1')
+                    board[i][j]='O';
+            }
+        }
+
+    }
+
+    public void Num130helper(char[][] board,int i, int j,int n,int m)
+    {
+        if(i<0||i>=n||j<0||j>=m)
+            return;
+        if(board[i][j]=='O')
+        {
+            board[i][j] = '1';
+            Num130helper(board,i+1,j,n,m);
+            Num130helper(board,i-1,j,n,m);
+            Num130helper(board,i,j+1,n,m);
+            Num130helper(board,i,j-1,n,m);
+        }
+        return;
+    }
+
+//    public void Num286wallsAndGates(int[][] rooms) {
+//        if(rooms.length==0||rooms[0].length==0)
+//            return ;
+//        int n = rooms.length;
+//        int m = rooms[0].length;
+//        for(int i=0;i<n;i++)
+//        {
+//            for(int j=0;j<m;j++)
+//            {
+//                if(rooms[i][j]==2147483647)
+//                {
+//                    Num286helper(rooms,i,j);
+//                }
+//            }
+//        }
+//
+//    }
+
+//    public int Num286helper(int[][] rooms,int i, int j)
+//    {
+//        if(i<0||i>rooms.length-1||j<0||j>rooms.length-1)
+//        {
+//            return -1;
+//        }
+//        if(rooms[i][j]==-1)
+//            return -1;
+//        if(rooms[i][j]==0)
+//            return 0;
+//        else if(rooms[i][j]==2147483647)
+//        {
+//            rooms[i][j] = -2;
+//            int up = Num286helper(rooms,i-1,j);
+//            int down = Num286helper(rooms,i+1,j);
+//            int left = Num286helper(rooms,i,j-1);
+//            int right = Num286helper(rooms,i,j+1);
+//            int finval = Integer.MAX_VALUE;
+//            finval = up ==-1? finval:Math.min(finval,up+1);
+//            finval = down==-1? finval:Math.min(finval,down+1);
+//            finval = left == -1?finval:Math.min(finval,left+1);
+//            finval = right == -1? finval:Math.min(finval,right+1);
+//            rooms[i][j] = finval;
+//            return finval;
+//        }
+//        return -1;
+//    }
+
+
+    public int Num127ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        int step = 0;
+        while(!queue.isEmpty())
+        {
+            int number = queue.size();
+            step+=1;
+            for(int i=0;i<number;i++)
+            {
+                String popedword = queue.poll();
+                if(popedword.equals(endWord))
+                    return step;
+                for(int j=0;j<wordList.size();)
+                {
+                    if(Num127helper(popedword,wordList.get(j)))
+                    {
+                        queue.add(wordList.get(j));
+                        wordList.remove(j);
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    public boolean Num127helper(String word1,String word2)
+    {
+        int diff= 0 ;
+        for(int i=0;i<word1.length();i++)
+        {
+            if(word1.charAt(i)!=word2.charAt(i))
+                diff++;
+        }
+        return diff==1;
+    }
+
+
+    public List<List<String>> Num51solveNQueens(int n) {
+        List<List<String>> ans = new LinkedList<>();
+        if(n==0)
+            return ans;
+        int[] poscounter = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            poscounter[0] = i;
+            Num51helper(n,poscounter,ans,1);
+        }
+        return ans;
+    }
+
+    public void Num51helper(int n, int[] poscounter, List<List<String>> ans,int depth)
+    {
+        if(depth==n)
+        {
+            List<String> solstr = new LinkedList<>();
+            for(int i=0;i<n;i++)
+            {
+                String curline = "";
+                for(int j=0;j<n;j++)
+                {
+                    if(poscounter[i]==j)
+                    {
+                        curline+="Q";
+                    }
+                    else
+                        curline+=".";
+                }
+                solstr.add(curline);
+            }
+            ans.add(solstr);
+            return;
+        }
+        int[] curpos = new int[n];
+        for(int i=0;i<depth;i++)
+        {
+            int colnum = poscounter[i];
+            curpos[colnum] = 1;
+            int k = depth-i;
+
+            if(colnum-k>=0)
+                curpos[colnum-k] = 1;
+            if(colnum+k<n)
+                curpos[colnum+k] = 1;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(curpos[i]==0)
+            {
+                poscounter[depth] = i;
+                Num51helper(n,poscounter,ans,depth+1);
+                poscounter[depth] = 0;
+            }
+        }
+    }
+
+    public List<List<String>> Num126findLadders(String beginWord, String endWord, List<String> wordList) {
+
+    }
+
+
+
     public void Num289gameOfLife(int[][] board) {
 
     }
